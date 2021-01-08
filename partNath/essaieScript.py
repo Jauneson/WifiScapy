cfcbae599f9edfbc870897888a7dbef560431ed7
NathanVaty <nathanvaty@gmail.com>
2021-01-08 20:08:38 +0000

    on essaie l'affichage des paquets

--- partNath/essaieScript.py
+++ partNath/essaieScript.py
@@ -6,14 +6,18 @@ import signal
 import os
 
 def actionPacket(packets):
-    print(packet[0])
-    #for packet in packets:
-       # print(packet.show())
-
+   for packet in packets:
+       print(packet.show())
+       print(packet.show2())
+       #print(packet[Dot11Elt].info)
 
 if __name__ == "__main__":
     os.system('ifconfig wlan0 down')
     os.system('iwconfig wlan0 mode monitor')
     os.system('ifconfig wlan0 up')
-    sniff(prn=actionPacket, iface="wlan0")
+    global ssid_list
+    ssid_list ={}
+    global s
+    s = conf.L2socket(iface="wlan0")
+    sniff(prn=actionPacket, iface="wlan0", store=0)
 
